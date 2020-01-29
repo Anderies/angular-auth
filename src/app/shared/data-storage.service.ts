5add39a17b1f7b48229a8d0cf0c824e5dd131a63
anderies <anderies@sprout.co.id>
2020-01-30 00:30:15 +0700

    Adding the token to outgoing request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
@@ -25,23 +25,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    // TAKE OPERATOR JUST GET ONCE AND AUTOMATICALLY UNSUBSCRIBE
-    return this.authService.user.pipe(
-      take(1), 
-      exhaustMap(user => {
-      return this.http.get<Recipe[]>(
-          'https://ng-complete-guide-d628c.firebaseio.com/recipes.json',{
-            params: new HttpParams().set('auth',user.token)
-          }
-        );
-    }), map(recipes => {
-      return recipes.map(recipe => {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients ? recipe.ingredients : []
-        };
-      });
-    }),
+    return this.http.get<Recipe[]>(
+      'https://ng-complete-guide-d628c.firebaseio.com/recipes.json'
+    ).pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
       tap(recipes => {
         this.recipeService.setRecipes(recipes);
       })
